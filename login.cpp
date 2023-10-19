653a7eace57581aaffac071a8ce50c0e59bc5638
Finnbar Home <finnbar.home2@gmail.com>
2023-10-19 13:23:56 +0100

    Started some basic inputs on the login.cpp

--- login.cpp
+++ login.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "authlib.h"
+#include "openssl/sha.h"
+
+// Function to get the SHA256 hash for a string provided o it
+std::string getSHA256(const std::string& input) {
+    // Implement SHA256 hash using openssl
+    // Return hash as string
+}
+
 
 int main() {
-  bool auth = true;
- 
-  if (auth) authenticated("user");
-  else rejected("user");
+    // Stores the password and username the user enters
+    string usernameIn, passwordIn;
+
+    // Prompt user for username and store
+    cout << "Enter your username: ";
+    cin >> usernameIn;
+
+    // Prompt user for password and store
+    cout << "Enter your password: ";
+    cin >> passwordIn;
 }
