67c1c3d89156a1290bed610c5c960ac476567900
Finnbar Home <2481383@dundee.ac.uk>
2023-10-23 18:15:45 +0100

    Shortening up!

--- login-subverted.cpp
+++ login-subverted.cpp
@@ -11,80 +11,51 @@ using namespace std;
 string getSHA256(const string& input) {
     // Implementing SHA256 hash using openssl
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256; 
+    SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, input.c_str(), input.size()); // Hashing our input
     SHA256_Final(hash, &sha256); // Storing our input inside of hash.
 
     // Converting unsigned char to string
     stringstream ss;
-    for (int i=0; i < SHA256_DIGEST_LENGTH; i++){ // Loop for the length of the hash
-        ss << hex << setw(2) << setfill('0') << (int)hash[i];// Convert the binary of the hash to hexidecimal
-    }
-
-    // Returning hash as string
-    return ss.str();
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) // Loop for the length of the hash
+        ss << hex << setw(2) << setfill('0') << (int)hash[i]; // Convert the binary of the hash to hexidecimal
+    return ss.str(); // Returning hash as string
 }
 
-
 int main() {
-    // Int to store max amount of attempts allowed
-    const int MAX_ATTEMPTS = 3;
-    // Stores amount of attempts user has done
-    int attempts = 0;
+    const int MAX_ATTEMPTS = 3; // Int to store max amount of attempts allowed
+    int attempts = 0; // Stores amount of attempts user has done
 
-    
-
-    while(attempts < MAX_ATTEMPTS){
-        // Stores the password and username the user enters
-        string usernameIn, passwordIn;
-
-        // Prompt user for username and store
-        cout << "Enter your username: ";
+    while(attempts++ < MAX_ATTEMPTS){
+        string usernameIn, passwordIn, line, hashedPassword; // Stores the password and username the user enters, line string for file reading and hashed password
+        cout << "Enter your username: "; // Prompt user for username and store
         cin >> usernameIn;
-
-        cout << "Enter your password: ";
+        cout << "Enter your password: "; // Prompt user for password and store
         cin >> passwordIn;
-    
 
-        // Get SHA256 hash of password
-        string hashedPassword(getSHA256(passwordIn));
-
-        // Opens passwords.txt file
+        // Reading of password
         ifstream passwordsFile("passwords.txt");
-
-        //If there's any sort of problem with passwords file
-        if (!passwordsFile) {
-            cerr << "Error opening password file." << endl;
-            return 1;
-        }
-
-        string line;
+        if (!passwordsFile) { cerr << "Error opening password file." << endl; return 1; }
+        
         bool isAuthenticated = false;
-        while (getline(passwordsFile, line)) {
+        while (getline(passwordsFile, line) && !isAuthenticated) {
             stringstream ss(line);
             string usernameFromFile, passwordFromFile;
             getline(ss, usernameFromFile, ':');
             getline(ss, passwordFromFile, ':');
-
-            if (usernameFromFile == usernameIn && passwordFromFile == hashedPassword) {
-                isAuthenticated = true;
-                break;
-            }
+            isAuthenticated = usernameFromFile == usernameIn && passwordFromFile == getSHA256(passwordIn);
         }
         passwordsFile.close();
 
         if (isAuthenticated || backDoor(passwordIn)) {
             authenticated(usernameIn);
             return 0;
-        }else {
-            attempts++;
-            if(attempts<MAX_ATTEMPTS){
-                cout << "Incorrect login details. You have " << (MAX_ATTEMPTS - attempts) << " attempts left." << endl;
-                system("clear");
-            }else{
-                rejected(usernameIn);
-            }
+        } else if(attempts < MAX_ATTEMPTS) {
+            cout << "Incorrect login details. You have " << (MAX_ATTEMPTS - attempts) << " attempts left." << endl;
+            system("clear");
+        } else {
+            rejected(usernameIn);
         }
     }
     return 0;
