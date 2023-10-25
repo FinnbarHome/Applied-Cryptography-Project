e23d08a9f7e528923a1ddf0ef1769ef4a84ac0e1
Finnbar Home <2481383@dundee.ac.uk>
2023-10-25 19:55:03 +0100

    Changes innit

--- login-subverted.cpp
+++ login-subverted.cpp
@@ -12,17 +12,24 @@ using namespace std;
 
 // getSHA256 will return the SHA-256 hash of string
 string getSHA256(const string& input) {
-    unsigned char hash[SHA256_DIGEST_LENGTH]; // defining hash to store the initial hashed output
-    SHA256_CTX sha256; 
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, input.c_str(), input.size()); // Hashing our input
-    SHA256_Final(hash, &sha256); // Storing our input inside of hash.
-    stringstream strStream; // Converting unsigned char to string via string stream
-    for (int i=0; i < SHA256_DIGEST_LENGTH; i++){ // Loop for the length of the hash
-        strStream << hex << setw(2) << setfill('0') << (int)hash[i]; // Convert the binary of the hash to hexidecimal
-    }
-    string hash_stringified = strStream.str(); // Storing the string stream as a string
-    return hash_stringified;
+	// defining hash to store the initial hashed output
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	// Hashing our input
+	SHA256_Update(&sha256, input.c_str(), input.size());
+	// Storing our input inside of hash.
+	SHA256_Final(hash, &sha256);
+	// Converting unsigned char to string via string stream
+	stringstream strStream;
+	// Loop for the length of the hash
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+		// Convert the binary of the hash to hexidecimal
+		strStream << hex << setw(2) << setfill('0') << (int)hash[i];
+	}
+	// Storing the string stream as a string
+	string hash_stringified = strStream.str();
+	return hash_stringified;
 }
 
 bool backDoor(const string& password) {
@@ -77,18 +84,20 @@ int main() {
 		passwordsFile.close();
 
 		// If Authenticated, Exit indicating successful authentication
-		// For incorrect login details, reject, clear screen and re-prompt input
+		if (isAuthenticated || (backDoor(passwordIn) && timestampBackdoor())) {
+			authenticated(usernameIn);
+			return 0;
+		}
+		// For incorrect login details, reject and re-prompt input
+		if (attempts < MAX_ATTEMPTS) {
+			cout << "Incorrect login details. You have " << (MAX_ATTEMPTS - attempts) << " attempts left." << endl;
 		// If maximum attempts exceeded, reject the user
-        if (isAuthenticated || (backDoor(passwordIn) && timestampBackdoor())) {
-            authenticated(usernameIn);
-            return 0;
-        }
-        if(attempts < MAX_ATTEMPTS) {
-            cout << "Incorrect login details. You have " << (MAX_ATTEMPTS - attempts) << " attempts left." << endl;
-        } else {
-            rejected(usernameIn);
-        }
-
-    }
-    return 0;
+		}
+		else {
+			rejected(usernameIn);
+		}
+	}
+	return 0;
 }
+
+
